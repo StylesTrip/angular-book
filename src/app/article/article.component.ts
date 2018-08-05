11d60bb8539bb1d4bdc1db00a5e9108476715da5
StylesTrip <rturinsky@pillartechnology.com>
2018-08-04 22:20:26 -0400

    Initial commit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model';
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cassClass = 'row';
+  article: Article;
+
+  constructor() {
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
